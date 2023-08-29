03b535f1da159507faffa10d10d364acbe15c6e8
kashifanwar313 <kashifanwar2015@gmail.com>
2023-08-29 15:35:00 +0500

    Dashboard initial design

--- src/app/dashboard/dashboard.js
+++ src/app/dashboard/dashboard.js
@@ -1,39 +1,25 @@
 "use client";
 import React from "react";
 import styles from "./page.module.css";
-import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
-import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import Sidebar from "@/components/sidebar/Sidebar";
+import Navbar from "@/components/navbar/Navbar";
+import Widget from "@/components/widgets/Widget";
 
 const Dashboard = () => {
   return (
-    <div className={styles.gridContainer}>
-        <header className={styles.header}>
-            <div className={styles.menuIcon}>
-                <MenuOutlinedIcon fontSize="large" />     
-            </div>  
+    <div className={styles.home}>
+      <Sidebar />
 
-            <div className={styles.headerLeft}>
-               <SearchOutlinedIcon fontSize="large" />
-            </div>
-
-            <div className={styles.headerRight}>
-                <NotificationsActiveOutlinedIcon className={styles.materialIconsOutlined} />
-                <EmailOutlinedIcon className={styles.materialIconsOutlined} />
-                <AccountCircleOutlinedIcon className={styles.materialIconsOutlined} />
-            </div>
-
-        </header>
-
-        <aside className={styles.sidebar}>
-
-        </aside>
-
-        <main className={styles.mainContainer}>
-
-        </main>
+      <div className={styles.homeContainer}>
+        <Navbar />
+        
+        <div className={styles.widgets}>
+            <Widget />
+            <Widget />
+            <Widget />
+            <Widget />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/widgets/Widget.js
@@ -0,0 +1,16 @@
+import './widget.css';
+
+const Widget = () => {
+  return (
+    <div className='widget'>
+        <div className='left'>
+            Left
+        </div>
+        <div className='right'>
+            Right
+        </div>
+    </div>
+  )
+}
+
+export default Widget
